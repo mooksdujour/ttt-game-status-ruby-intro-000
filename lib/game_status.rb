6778c1bb17d2a2cfa94458bd6904f4a6df234996
John Moo-King <jmooking@hotmail.com>
2019-05-09 16:16:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,28 +20,12 @@ def won?(board)
      win_index_1 = win_combination[0]
      win_index_2 = win_combination[1]
      win_index_3 = win_combination[2]
-     win_index_4 = win_combination[3]
-     win_index_5 = win_combination[4]
-     win_index_6 = win_combination[5]
-     win_index_7 = win_combination[6]
-     win_index_8 = win_combination[7]
 
      position_1 = board[win_index_1] # value of board at win_index_1
      position_2 = board[win_index_2] # value of board at win_index_2
      position_3 = board[win_index_3] # value of board at win_index_3
-     position_4 = board[win_index_4] # value of board at win_index_4
-     position_5 = board[win_index_5] # value of board at win_index_5
-     position_6 = board[win_index_6] # value of board at win_index_6
-     position_7 = board[win_index_7] # value of board at win_index_7
-     position_8 = board[win_index_8] # value of board at win_index_8
-     if ((position_1 == "X" || position_1 == "O") && position_taken?(board, win_index_1)) ||
-        ((position_2 == "X" || position_2 == "O") && position_taken?(board, win_index_2)) ||
-        ((position_3 == "X" || position_3 == "O") && position_taken?(board, win_index_3)) ||
-        ((position_4 == "X" || position_4 == "O") && position_taken?(board, win_index_4)) ||
-        ((position_5 == "X" || position_5 == "O") && position_taken?(board, win_index_5)) ||
-        ((position_6 == "X" || position_6 == "O") && position_taken?(board, win_index_6)) ||
-        ((position_7 == "X" || position_7 == "O") && position_taken?(board, win_index_7)) ||
-        ((position_8 == "X" || position_8 == "O") && position_taken?(board, win_index_8))
+
+     if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_2)
        return win_combination
      end
    end
